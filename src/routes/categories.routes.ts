b0caf1af6de35e5b66983f16f39bafe7ece1b5ef
Muriel Damazio <murieldps@gmail.com>
2022-01-31 19:55:29 -0300

    Creating importCategory feature. Starting w/ Stream on Node

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,30 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/CreateCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listAllCategoriesController } from "../modules/cars/useCases/ListAllCategories";
 
 
 const categoriesRoutes = Router();
 
+const upload = multer({
+	dest:"./tmp"
+});
+
 //Routes
 categoriesRoutes.post("/categories", (request, response) => {
 	return createCategoryController.handle(request, response);
 });
+
 categoriesRoutes.get("/categories", (request, response) => {
 	listAllCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post("/categories/import", upload.single("file"), (request, response) => {
+	return importCategoryController.handle(request, response);
+});
+
 
 export { categoriesRoutes };
 
