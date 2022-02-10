b9e7d0b0730023cdaabf35f8ad56987cb89cd2ad
Muriel Damazio <murieldps@gmail.com>
2022-02-10 09:32:34 -0300

    Config Postgres DB on Docker. Typeorm categories migration done

--- src/modules/cars/models/Category.ts
+++ src/modules/cars/models/Category.ts
@@ -1,11 +1,20 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("categories")
 class Category {
 	
-	id?: string;
-	name: string;
-	description: string;
-	created_at: Date;
+	@PrimaryColumn()
+		id?: string;
+	
+	@Column()
+		name: string;
+
+	@Column()
+		description: string;
+	
+	@CreateDateColumn()
+		created_at: Date;
 
 	constructor() {
 		if (!this.id) {
