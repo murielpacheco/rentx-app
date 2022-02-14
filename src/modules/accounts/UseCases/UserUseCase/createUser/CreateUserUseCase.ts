bba906ba65f3ebaae6cbf08c62ed34c3a4804be4
Muriel Damazio <murieldps@gmail.com>
2022-02-14 09:21:53 -0300

    Finishing createUserUseCase/Controller. Password cryptography with bcryptjs

--- src/modules/accounts/UseCases/UserUseCase/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/UserUseCase/createUser/CreateUserUseCase.ts
@@ -1,6 +1,8 @@
-import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../../repositories/IUsersRepository";
+
+import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 
 
 @injectable()
@@ -10,8 +12,21 @@ class CreateUserUseCase {
       private usersRepository: IUsersRepository) { } 
    
 	async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+		const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+		if (userAlreadyExists) {
+			throw new Error ("User already exists!");
+		}
+
+		const passwordHash = await hash(password, 8);
       
-		await this.usersRepository.create({ name, email, password, driver_license });
+		await this.usersRepository.create({
+			name,
+			email,
+			password: passwordHash,
+			driver_license
+		});
 	}
 }
 
