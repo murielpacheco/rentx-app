a5f6eba772d7cb9bae269e1d3cd8bf9fc14bd66d
Muriel Damazio <murieldps@gmail.com>
2022-02-22 19:34:52 -0300

    Fix: middleware ensureAuth

--- src/shared/infra/http/middlewares/ensureAuth.ts
+++ src/shared/infra/http/middlewares/ensureAuth.ts
@@ -1,42 +1,33 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 
 interface IPayload {
-   sub: string;
-}
-
-export async function ensureAuth(request: Request, response: Response, next: NextFunction) {
-	const token = request.headers.authorization;
-   
-	if (!token) { 
-		throw new AppError("No token provided", 401);
+	sub: string;
+ }
+ 
+export async function ensureAuth(
+	request: Request,
+	response: Response,
+	next: NextFunction
+) {
+	const authHeader = request.headers.authorization;
+ 
+	if (!authHeader) {
+	  throw new AppError("Token missing", 401);
 	}
-   
-	const [scheme, tokenValue] = token.split(" ");
-
-	if (scheme !== "Bearer") {
-		throw new AppError("Invalid token");
-	}
-   
+ 
+	const [, token] = authHeader.split(" ");
+ 
 	try {
-		const { sub: user_id } = verify(tokenValue, process.env.JWT_SECRETKEY) as IPayload;
-      
-		const usersRepository = new UsersRepository();
-      
-		const user = await usersRepository.findById(user_id);
-      
-		if (!user) {
-			throw new AppError("Invalid user", 401);
-		}
-
-		request.user = {
-			id: user_id,
-		};
-		return next();
-	} catch (error) {
-		throw new AppError("Invalid token!", 401 );
+	  const { sub: user_id } = verify(token, "lolalwleleel") as IPayload;
+ 
+	  request.user = {
+		 id: user_id,
+	  };
+ 
+	  next();
+	} catch {
+	  throw new AppError("Invalid token!", 401);
 	}
-
 }
